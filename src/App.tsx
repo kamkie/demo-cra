d1b4a642b98ac0f465ddd024720661b809bd2311
Kamil Kiewisz <kamkie@outlook.com>
2019-03-01 23:59:09 +0100

    clean code

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import './App.css';
 import {ConnectedRouter} from "connected-react-router";
 import configureStore, {history} from "./store";
@@ -8,22 +8,20 @@ import {Home} from "./pages/Home";
 
 const store = configureStore({});
 
-export default class App extends Component {
-  render() {
-    return <Provider store={store}>
-      <ConnectedRouter history={history}>
-        <Switch>
-          <Route exact path="/">
-            <Home/>
-          </Route>
-          <Route exact path="/foo">
-            <Home message={<div>hello</div>}/>
-          </Route>
-          <Route>
-            <div>Miss</div>
-          </Route>
-        </Switch>
-      </ConnectedRouter>
-    </Provider>
-  }
-}
+export default function App() {
+  return <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route exact path="/foo">
+          <Home message={<div>hello</div>}/>
+        </Route>
+        <Route>
+          <div>Miss</div>
+        </Route>
+      </Switch>
+    </ConnectedRouter>
+  </Provider>
+};
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 import {Link} from "react-router-dom";
 import React from "react";
 
-
 export default function Navbar() {
   return <>
     <Link to="/">Home</Link>
