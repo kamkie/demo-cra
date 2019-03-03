f493d8d726219b178fb792ae3a5b58bbfba28cb7
Kamil Kiewisz <kamkie@outlook.com>
2019-03-03 01:06:22 +0100

    redux ajax request - no thunk or sagas

--- src/components/ReduxCounter.tsx
+++ src/components/ReduxCounter.tsx
@@ -1,35 +1,40 @@
-import React from 'react';
+import React, {CSSProperties} from 'react';
 import {connect} from "react-redux";
 import {Action, Dispatch} from "redux";
 import {AppState} from "../store";
 
+const contextPath = process.env.REACT_APP_CONTEXT_PATH === '/' ? '' : process.env.REACT_APP_CONTEXT_PATH || '';
+
 const initialState: CounterState = {
-  count: 0
+  count: 0,
+  loading: false,
+  data: {}
 };
 
 export interface CounterState {
   readonly count: number
+  readonly loading: boolean
+  readonly data: object
 }
 
 interface CounterAction extends Action<CounterActions> {
+  payload: object
 }
 
 interface CounterExternalProps {
   storeSelector: (store: AppState) => CounterState
 }
 
-interface CounterStateProps {
-  readonly count: number
-}
-
 interface CounterDispatchProps {
   readonly increment: () => void
   readonly decrement: () => void
+  readonly fetch: () => void
+  readonly dataLoaded: (data: object) => void
 }
 
-type CounterProps = CounterExternalProps & CounterStateProps & CounterDispatchProps
+type CounterProps = CounterExternalProps & CounterState & CounterDispatchProps
 
-function mapStateToProps(state: AppState, ownProps: CounterExternalProps): CounterStateProps {
+function mapStateToProps(state: AppState, ownProps: CounterExternalProps): CounterState {
   return ownProps.storeSelector(state);
 }
 
@@ -37,6 +42,8 @@ function mapDispatchToProps(dispatch: Dispatch<Action<CounterActions>>): Counter
   return {
     increment: () => dispatch({type: CounterActions.INCREMENT}),
     decrement: () => dispatch({type: CounterActions.DECREMENT}),
+    fetch: () => dispatch({type: CounterActions.LOAD}),
+    dataLoaded: data => dispatch({type: CounterActions.LOADED, payload: data}),
   }
 }
 
@@ -44,17 +51,31 @@ enum CounterActions {
   INCREMENT = '@@counter/INCREMENT',
   DECREMENT = '@@counter/DECREMENT',
   // DECREMENT2 = '@@counter/DECREMENT2',
+  LOAD = '@@counter/LOAD',
+  LOADED = '@@counter/LOADED',
 }
 
-export function counterReducer(state = initialState, action: CounterAction) {
-  const {INCREMENT, DECREMENT} = CounterActions;
+export function counterReducer(state = initialState, action: CounterAction): CounterState {
   switch (action.type) {
-    case INCREMENT:
+    case CounterActions.LOAD:
       return {
+        ...state,
+        loading: true
+      };
+    case CounterActions.LOADED:
+      return {
+        ...state,
+        data: action.payload,
+        loading: false
+      };
+    case CounterActions.INCREMENT:
+      return {
+        ...state,
         count: state.count + 1
       };
-    case DECREMENT:
+    case CounterActions.DECREMENT:
       return {
+        ...state,
         count: state.count - 1
       };
     default:
@@ -68,10 +89,24 @@ export function assertNever(_: never) {
   console.error("Never touched");
 }
 
+const style: CSSProperties = {
+  textAlign: 'left',
+  color: '#ffffff',
+};
+
 class ReduxCounter extends React.Component<CounterProps> {
 
+  loadData = () => {
+    this.props.fetch();
+    fetch(`${contextPath}/actuator/info`)
+      .then(response => response.json())
+      .then(info => info.build)
+      .then(body => this.props.dataLoaded(body))
+      .catch(error => this.props.dataLoaded(error))
+  };
+
   render() {
-    const {count, increment, decrement} = this.props;
+    const {count, increment, decrement, loading, data} = this.props;
     return (
       <div>
         <h2>Counter</h2>
@@ -79,6 +114,9 @@ class ReduxCounter extends React.Component<CounterProps> {
           <button onClick={decrement}>-</button>
           <span>count: {count}</span>
           <button onClick={increment}>+</button>
+          <button onClick={this.loadData}>fetch</button>
+          <p>{loading && <span>loading data</span>}</p>
+          <pre style={style}>{JSON.stringify(data, null, 4)}</pre>
         </div>
       </div>
     )
